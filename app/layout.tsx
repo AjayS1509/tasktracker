728152918b7a3825dc17f92384a58a0b0c654893
Ajay Soni <ajaypsoni1509@gmail.com>
2024-07-26 11:54:47 +0530

    meta added

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,57 @@ import Footer from "@/components/layout/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://task-management-system-alpha.vercel.app'),
   title: "Task Management System",
   description:
     "A task management system is a web application designed to help users organize and track tasks efficiently. Users can add tasks along with titles, descriptions, and status indicators, enabling them to prioritize, manage progress, and keep track of task completion.",
+  keywords: [
+    "Resume",
+    "Front-End Developer Resume",
+    "Web Developer Resume",
+    "Ajay Soni Resume",
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Task Management System",
+    "Organize Tasks",
+    "Track Tasks",
+    "Task Titles",
+    "Task Descriptions",
+    "Status Indicators",
+    "Task Prioritization",
+    "Task Progress",
+    "Task Completion",
+    "Efficient Task Tracking",
+    "Task Organization",
+    "Task Management Web App",
+    "User Task Tracking",
+    "Task Management Tools",
+    "Productivity App",
+    "Task Monitoring",
+    "Task Workflow",
+    "Manage Tasks Online",
+    "Task Tracking Software",
+    "Task Planner"
+  ],
+  authors: [{ name: "Ajay Soni" }],
+  openGraph: {
+    title: "Task Management System",
+    description:
+      "A task management system is a web application designed to help users organize and track tasks efficiently. Users can add tasks with titles, descriptions, and status indicators, enabling them to prioritize, manage progress, and track completion. The system includes Google login managed by NextAuth and uses MongoDB Cloud for data storage.",
+    images: [
+      {
+        url: "./task.png",
+        alt: "Task management System Photo",
+      },
+    ],
+    url: "https://task-management-system-alpha.vercel.app",
+    siteName: "Task",
+    locale: "en-us"
+  },
+  icons:{
+    icon:'./task.ico',
+  }
 };
 
 export default function RootLayout({
